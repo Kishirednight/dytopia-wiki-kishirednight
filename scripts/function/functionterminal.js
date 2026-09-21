6d6fb8135763cdbe63afb5cde89cbd3b6742a610
Kishirednight <kishirednight@gmail.com>
2026-09-21 17:45:33 +0200

    Création de la commande run et préparation des déplacements des fenetres

--- scripts/function/functionterminal.js
+++ scripts/function/functionterminal.js
@@ -1,4 +1,5 @@
 async function boucle() {
+    await WriteTerminal("")
     ADDinput()
     const reponse = await ListenInput()
 
@@ -7,8 +8,46 @@ async function boucle() {
     } if (!reponse) {
         await WriteTerminal("Rentrer une commande valide.")
     } else {
-        await WriteTerminal(`Commande écrite: ${reponse}`)
+        await distribuer(reponse)
     }
 
     boucle()
+}
+
+async function distribuer(reponse) {
+    const commande = reponse.split(" ")[0].trim().toLowerCase()
+    const parametre = reponse.split("").slice(1)
+
+    if (commande === "run") {
+        await run(parametre)
+    }
+}
+
+async function run(fichier) {
+    // Ouvre un document et son contenu
+
+    const fenetre = document.getElementById("fenetre")
+
+    // === Gestion d'une vrai fenetre === \\\
+    let deplacement = false
+    let decalageX = 0
+    let decalageY = 0
+
+    fenetre.addEventListener("mousedown", (event) => {
+        deplacement = true
+        decalageX = event.clientX - fenetre.offsetLeft
+        decalageY = event.clientY - fenetre.offsetTop
+    })
+    document.addEventListener("mousemove", (event) => {
+        if (!deplacement) return
+
+        fenetre.style.left = (event.clientX - decalageX) + "px"
+        fenetre.style.top = (event.clientY - decalageY) + "px"
+    })
+    document.addEventListener("mouseup", () => {
+        deplacement = false
+    })
+
+    // === Gestion du contenu === \\
+    fenetre.style.display = "block"
 }
